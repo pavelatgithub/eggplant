df88b96249f8dc0c8ee761398a62639a1d77532d
Pavlo Yakovchuk <Pavlo_Yakovchuk@epam.com>
2018-07-08 18:30:44 +0300

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=eggplantapp-Web-Deploy.pubxml -p:Password=uyHttbq1oQAiJjFWNquDi5m6P2G7EhlhDb8pR0ZbbtEkZZtxWe8oFcaDCj8Q
+
